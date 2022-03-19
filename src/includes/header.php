0aefc0d20cccaccdf212c607c4855d4c70b5b929
Balazs Nyirfa <balazsnyirfa@gmail.com>
2022-03-19 19:49:26 +0100

    add header & home page

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,38 +1,53 @@
 <header>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-      </ul>
-      <?php 
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="todoPage.php">Todo</a>
+          </li>
+        </ul>
+        <?php
+        if (isset($_COOKIE['userName'])) {
+          $userNameFromCookie = $_COOKIE['userName'];
+          $checkRemember = true;
+        } else {
+          $userNameFromCookie = "";
+          $checkRemember = false;
+        }
         if (!isset($_SESSION['userName'])) {
           echo '
-          <form class="d-flex" action="./user/login.php" method="POST">
-            <input class="form-control me-2" type="text" placeholder="username" aria-label="username" name="userName">
-            <input class="form-control me-2" type="password" placeholder="password" aria-label="password" name="password">
+          <form class="d-sm-flex" action="./user/login.php" method="POST">
+            <p class="w-100 text-warning">remember user name?</p>';
+          if ($checkRemember == true) {
+            echo '<input type="checkbox" name="rememberUserName" id="rememberUserName" class="me-2 mt-2" checked>';
+          } else {
+            echo '<input type="checkbox" name="rememberUserName" id="rememberUserName" class="me-2 mt-2">';
+          }
+          echo '<input class="form-control me-2 text-warning border-warning" type="text" placeholder="userName" aria-label="userName" name="userName" id="userName"
+              value="' . $userNameFromCookie . '"
+            >
+            <input class="form-control me-2" type="password" placeholder="password" aria-label="password" name="password" id="password">
             <input class="btn btn-success" type="submit" value="LogIn">
           </form>
           ';
-        }
-        else {
+        } else {
           echo '
-          <form action="logout.php">
+          <p class="me-5">Logged in as: <span class="text-info">' . $_SESSION['userName'] . '</span></p>
+          <form action="./user/logout.php" method="POST">
             <input class="btn btn-danger" type="submit" value="LogOut">
           </form>
           ';
         }
-      ?>
+        ?>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </header>
--- src/index.php
+++ src/index.php
@@ -1,11 +1,16 @@
-<?php 
-  include('./includes/head.php');
+<?php
+include('./includes/head.php');
 ?>
 
 <body>
-<?php 
+  <?php
   include('./includes/header.php');
-?>
+  ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <div class="p-3">
+    <p class="text-warning">Yellow = Cookie</p>
+    <p class="text-info">Blue = session</p>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
